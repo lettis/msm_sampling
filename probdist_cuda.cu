d649931e1e69eb9f7a34e044a42e887977db0d42
Florian Sittel <sittel@lettis.net>
2017-03-01 23:07:44 +0100

    more work on cuda code

--- probdist_cuda.cu
+++ probdist_cuda.cu
@@ -17,7 +17,12 @@ namespace CUDA {
     cudaGetDeviceCount(&n_gpus);
     check_error("trying to get number of available GPUs");
     if (n_gpus == 0) {
-      std::cerr << "error: no CUDA-compatible GPUs found" << std::endl;
+      std::cerr << "error: no CUDA-compatible GPUs found."
+                << std::endl
+                << "       if you are sure to have one,"
+                << std::endl
+                << "       check your device drivers!"
+                << std::endl;
       exit(EXIT_FAILURE);
     } else {
       return n_gpus;
@@ -34,38 +39,57 @@ namespace CUDA {
     gpu.id = i_gpu;
     gpu.n_dim = n_dim;
     gpu.states = states;
+    cudaSetDevice(i_gpu);
+    check_error("setting CUDA device");
     for (unsigned int state: states) {
       unsigned int split_size = fe[state].size();
       gpu.split_sizes[state] = split_size;
-      // reserve memory
+      //// reserve memory
       cudaMalloc((void**) &gpu.fe[state]
                , sizeof(float) * split_size);
       cudaMalloc((void**) &gpu.coords[state]
                , sizeof(float) * split_size * gpu.n_dim);
-      // copy data
+      check_error("device memory allocation");
+      //// copy data to device
       cudaMemcpy(gpu.fe[state]
                , fe[state].data()
                , sizeof(float) * split_size
                , cudaMemcpyHostToDevice);
-
-      //TODO coords
-
-
-
+      check_error("copying of state-splitted free energies");
+      // reference coords in 1D array (row-major order)
+      std::vector<float> tmp_coords(n_dim * split_size);
+      for (unsigned int i=0; i < split_size; ++i) {
+        for (unsigned int j=0; j < n_dim; ++j) {
+          tmp_coords[i*split_size+j];
+        }
+      }
+      cudaMemcpy(gpu.coords[state]
+               , tmp_coords.data()
+               , sizeof(float) * n_dim * split_size
+               , cudaMemcpyHostToDevice);
+      check_error("copying of state-splitted coordinates");
     }
     return gpu;
   }
 
   void
   clear_gpu(GPUSettings gpu) {
+    cudaSetDevice(gpu.id);
+    check_error("setting CUDA device");
     for (unsigned int state: gpu.states) {
       cudaFree(gpu.fe[state]);
+      check_error("freeing memory for free energies");
       cudaFree(gpu.coords[state]);
+      check_error("freeing memory for coordinates");
     }
   }
 
 
+  
 
+
+  //TODO reuse for FE estimate
+/*
   Pops
   calculate_populations_per_gpu(const float* coords
                               , std::size_t n_rows
@@ -156,20 +180,35 @@ namespace CUDA {
     cudaFree(d_pops);
     return pops;
   }
+*/
 
-  Pops
-  calculate_populations(const float* coords
-                      , const std::size_t n_rows
-                      , const std::size_t n_cols
-                      , std::vector<float> radii) {
-    using Clustering::Tools::dim1_sorted_coords;
-    using Clustering::Tools::boxlimits;
-    ASSUME_ALIGNED(coords);
-    std::sort(radii.begin(), radii.end(), std::greater<float>());
-    int n_gpus = get_num_gpus();
-    int gpu_range = n_rows / n_gpus;
+
+  std::pair<unsigned int, float>
+  fe_estimate_partial(const std::vector<float>& xs
+                    , unsigned int state
+                    , unsigned int i_from
+                    , unsigned int i_to
+                    , GPUSettings& gpu) {
+    //TODO
+  }
+
+  float
+  fe_estimate(const std::vector<float>& xs
+            , unsigned int state
+            , const std::vector<GPUSettings>& gpus) {
+    int n_gpus = gpus.size();
+    if (n_gpus == 0) {
+      std::cerr << "error: unable to estimate free energies on GPU(s)."
+                << std::endl
+                << "       no GPUs have been provided."
+                << std::endl;
+      exit(EXIT_FAILURE);
+    }
+    unsigned int n_rows = gpus[0].split_sizes[state];
+    unsigned int gpu_range = n_rows / n_gpus;
     int i;
-    std::vector<Pops> partial_pops(n_gpus);
+    // partial estimates: pair of {#neighbors, sum(FE)}
+    std::vector<std::pair<unsigned int, float>> partial_estimates(n_gpus);
     #pragma omp parallel for default(none)\
       private(i)\
       firstprivate(n_gpus,n_rows,n_cols,gpu_range)\
@@ -177,29 +216,24 @@ namespace CUDA {
       num_threads(n_gpus)\
       schedule(dynamic,1)
     for (i=0; i < n_gpus; ++i) {
-      // compute partial populations in parallel
-      // on all available GPUs
-      partial_pops[i] = calculate_populations_per_gpu(coords
-                                                    , n_rows
-                                                    , n_cols
-                                                    , radii
-                                                    , i*gpu_range
-                                                    , i == (n_gpus-1)
-                                                        ? n_rows
-                                                        : (i+1)*gpu_range
-                                                    , i);
+      // use available GPUs in parallel to
+      // compute partial estimates
+      partial_estimates[i] = fe_estimate_partial(xs
+                                               , state
+                                               , i*gpu_range
+                                               , i == (n_gpus-1)
+                                                   ? n_rows
+                                                   : (i+1)*gpu_range
+                                               , gpus[i]);
     }
-    Pops pops;
-    // combine pops
-    for (float r: radii) {
-      pops[r].resize(n_rows, 0);
-      for (i=0; i < n_rows; ++i) {
-        for (unsigned int i_gpu=0; i_gpu < n_gpus; ++i_gpu) {
-          pops[r][i] += partial_pops[i_gpu][r][i];
-        }
-      }
+    // combine results
+    unsigned int n_neighbors = 0;
+    float fe_estimate = 0.0f;
+    for (auto result: partial_estimates) {
+      n_neighbors += result.first;
+      fe_estimate += result.second;
     }
-    return pops;
+    return fe_estimate / ((float) n_neighbors);
   }
 
 }
